2ffb4192aacb56c0e1c75b06abcc21119e9c2c17
ograsyilmaz <yilmazogras@outlook.com.tr>
2018-03-20 10:32:00 +0300

    webpack değişimi ve index js düzeltim

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path=require('path');
+const path = require('path');
 
-const config={
-    entry:'./lib/components/Index.js',
-    output:{
-        path:path.resolve(__dirname,'public'),
-        filename:'bundle.js'
+const config = {
+    entry: './lib/components/Index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
     },
-    module:{
-        rules:[
-            {test:/\.js$/,use:'babel-loader'}
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
         ]
     }
 };
 
-module.exports=config;
+module.exports = config;
