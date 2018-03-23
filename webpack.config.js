454f4f0d75ffd0d03b9a05ddb8a6ba3ddcddb3af
ograsyilmaz <yilmazogras@outlook.com.tr>
2018-03-23 22:14:19 +0300

    nodemon  refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 const path = require('path');
 
 const config = {
-    entry: ['babel-polyfill','./lib/components/Index.js'],
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
-        ]
-    }
+  resolve: {
+    modules: [
+      path.resolve('./lib'),
+      path.resolve('./node_modules'),
+    ]
+  },
+
+  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+    ]
+  }
 };
 
 module.exports = config;
